21d04cd1dd7c09c394c1fe470651c54d17920c29
Elias Kelta <eliaskelta1@gmail.com>
2022-01-19 18:33:50 +0100

    finished

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 let input = document.getElementById("input").value
-let base = document.getElementById("base").options.selectedIndex //0 = binary, 1 = octal, 2 = decimal, 3 = hexadecimal
-console.log(base)
+let basenum = document.getElementById("base").options.selectedIndex //0 = binary, 1 = octal, 2 = decimal, 3 = hexadecimal
+console.log(basenum)
+if (basenum == 0) {
+    var base = 2
+}else if (basenum == 1) {
+    var base = 8
+}else if (basenum == 2) {
+    var base = 10
+}else if (basenum == 3) {
+    var base = 16
+}
 
 //generate array [item 0, item1]
 if (input.includes("+")){
     var input_array = input.split("+")
-    let result = parseInt(input_array[0]) + parseInt(input_array[1])
-    document.getElementById("output").value = result
+    let result = parseInt(input_array[0], base) + parseInt(input_array[1], base)
+    console.log(result)
+    document.getElementById("output").value = result.toString(base)
 
 }else if (input.includes("-")){
     var input_array = input.split("-")
-    let result = parseInt(input_array[0]) - parseInt(input_array[1])
-    document.getElementById("output").value = result
+    let result = parseInt(input_array[0], base) - parseInt(input_array[1], base)
+    console.log(result)
+    document.getElementById("output").value = result.toString(base)
 
 }else if (input.includes("*")){
     var input_array = input.split("*")
-    let result = parseInt(input_array[0]) * parseInt(input_array[1])
-    document.getElementById("output").value = result
+    let result = parseInt(input_array[0], base) * parseInt(input_array[1], base)
+    console.log(result)
+    document.getElementById("output").value = result.toString(base)
 
 }else if (input.includes("/")){
     var input_array = input.split("/")
-    let result = parseInt(input_array[0]) / parseInt(input_array[1])
-    document.getElementById("output").value = result
+    let result = parseInt(input_array[0], base) / parseInt(input_array[1], base)
+    console.log(result)
+    document.getElementById("output").value = result.toString(base)
 
 }else {
     var input_array = []
