5c1ace0fe3a36a99721c76023d42c6086a4a6a77
Elias Kelta <eliaskelta1@gmail.com>
2022-01-20 15:06:29 +0100

    added base 2, 4 & 6 and added negative number support

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 function calc() {
     let input = document.getElementById("input").value
-    let basenum = document.getElementById("base").options.selectedIndex //0 = binary, 1 = octal, 2 = decimal, 3 = hexadecimal
+    let basenum = document.getElementById("base").options.selectedIndex //0 = binary, 1 = base 4, 2 = base 6, 3 = octal, 4 = decimal, 5 = hex.
     console.log(basenum)
     if (basenum == 0) {
         var base = 2
     }else if (basenum == 1) {
-        var base = 8
+        var base = 4
     }else if (basenum == 2) {
-        var base = 10
+        var base = 6
     }else if (basenum == 3) {
+        var base = 8
+    }else if (basenum == 4) {
+        var base = 10
+    }else if (basenum == 5) {
         var base = 16
     }
 
     //calculate
-    if (input.includes("+")){
-        var input_array = input.split("+")
-        let result = parseInt(input_array[0], base) + parseInt(input_array[1], base)
-        console.log(result)
-        document.getElementById("output").value = result.toString(base)
-
-    }else if (input.includes("-")){
-        var input_array = input.split("-")
-        let result = parseInt(input_array[0], base) - parseInt(input_array[1], base)
-        console.log(result)
-        document.getElementById("output").value = result.toString(base)
-
-    }else if (input.includes("*")){
+    if (input.includes("*")){
         var input_array = input.split("*")
         let result = parseInt(input_array[0], base) * parseInt(input_array[1], base)
+        console.log(result)
+        document.getElementById("output").value = result.toString(base)
+
+    }else if (input.includes("^")){
+        var input_array = input.split("^")
+        let result = parseInt(input_array[0], base) ** parseInt(input_array[1], base)
         console.log(result)
         document.getElementById("output").value = result.toString(base)
 
@@ -43,14 +41,25 @@ function calc() {
         console.log(result)
         document.getElementById("output").value = result.toString(base)
 
-    }else if (input.includes("^")){
-        var input_array = input.split("^")
-        let result = parseInt(input_array[0], base) ** parseInt(input_array[1], base)
+    }else if (input.includes("+")){
+        var input_array = input.split("+")
+        let result = parseInt(input_array[0], base) + parseInt(input_array[1], base)
+        console.log(result)
+        document.getElementById("output").value = result.toString(base)
+
+    }else if (input.includes("-")){
+        if (input.startsWith("-")) {
+            var input_array = input.slice(1).split("-")
+            input_array[0] *= -1
+        }else {
+            var input_array = input.split("-")
+        }
+        let result = parseInt(input_array[0], base) - parseInt(input_array[1], base)
         console.log(result)
         document.getElementById("output").value = result.toString(base)
 
     }else {
-        var input_array = []
+        var input_array = ["error"]
         document.getElementById("output").value = "Error"
     }
 
